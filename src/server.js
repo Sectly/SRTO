c69c88b6e822b39d3639150fbc53affb93dcc1ca
DanHowe0 <danhowe0@gmail.com>
2025-12-27 16:26:54 +0000

    fix: Update server port to 5000

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const DATA_PATH = process.env.DATA_PATH
   ? path.resolve(process.env.DATA_PATH)
   : path.join(__dirname, '../data');
